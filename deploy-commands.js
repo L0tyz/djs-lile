908a42c3a1e5bbbfe79c48cfb426e2ef82093772
L0tyz <l0tyz.pro@gmail.com>
2026-01-30 14:51:41 -0500

    Command Handler + Deployer

---
+++ deploy-commands.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[ATTENTION] La commande à l'emplacement ${filePath} ne comporte pas la propriété "data" ou "execute" requise.`);
+		}
+	}
+}
+
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+	try {
+		console.log(`Début du rafraîchissement des commandes ${commands.length}.`);
+
+		const data = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+
+		console.log(`Rechargement réussi des commandes ${data.length}.`);
+	} catch (error) {
+
+		console.error(error);
+	}
+})();
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -14,3 +16,47 @@ client.once('clientReady', () => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+client.commands = new Collection(); 
+
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[ATTENTION] La commande à l'emplacement ${filePath} ne comporte pas la propriété "data" ou "execute" requise.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`Aucune commande correspondant à ${interaction.commandName} trouver.`);
+		return;
+	}
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'Une erreur s\'est produite lors de l\'exécution de cette commande !',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: 'Une erreur s\'est produite lors de l\'exécution de cette commande !',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
+});
